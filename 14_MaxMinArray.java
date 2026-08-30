4af5c941e8adb038487c56e9431b60168577d01f
K. Karan Murugan <karankarthik1407@gmail.com>
2026-08-30 20:11:14 +0530

    Create 14_MaxMinArray.java

---
+++ 14_MaxMinArray.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class MaxMinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        // Find max and min
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Maximum = " + max);
+        System.out.println("Minimum = " + min);
+
+        sc.close();
+    }
+}
